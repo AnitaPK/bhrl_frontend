bc1c5e5522b7de23a703a2b59efda5f96bb34484
AnitaPK <anita.khamitkar@gmail.com>
2025-12-07 08:40:50 +0530

    url add

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const rawBase = import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000/api';
+const rawBase = process.env.VITE_API_BASE_URL || 'http://localhost:5000/api';
 const API_BASE_URL = rawBase.replace(/\/+$/, '') + '/api';
 
 const api = axios.create({
